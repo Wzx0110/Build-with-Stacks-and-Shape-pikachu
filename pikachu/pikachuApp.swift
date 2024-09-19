bc23db92c1e5be0b1511fc467eb6437aee1bf6ff
ZiXiang <wzxiang9301@gmail.com>
2024-09-19 14:24:57 +0800

    Initial Commit

---
+++ pikachu/pikachuApp.swift
@@ -0,0 +1,17 @@
+//
+//  pikachuApp.swift
+//  pikachu
+//
+//  Created by 翁子翔 on 2024/9/19.
+//
+
+import SwiftUI
+
+@main
+struct pikachuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pikachuTests/pikachuTests.swift
@@ -0,0 +1,17 @@
+//
+//  pikachuTests.swift
+//  pikachuTests
+//
+//  Created by 翁子翔 on 2024/9/19.
+//
+
+import Testing
+@testable import pikachu
+
+struct pikachuTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
